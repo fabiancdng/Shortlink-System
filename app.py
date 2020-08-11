8734856aa4db7d35e28d02385b389cb444194dd3
Fabian Reinders <fabian@fabiancdng.com>
2020-08-11 18:28:45 +0200

    creator is now identified by secret and not longer by email

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def get_shortlink(shortlink):
         link["id"] = row[0]
         link["short"] = row[1]
         link["redirect"] = row[2]
-        link["email"] = row[3]
+        link["secret"] = row[3]
         link["delay"] = row[4]
         link["created"] = row[5]
 
@@ -33,7 +33,7 @@ def get_shortlink(shortlink):
     else:
         return "error"
 
-def create_shortlink(longlink, email, delay):
+def create_shortlink(longlink, secret, delay):
     if not longlink.__contains__("https://") and not longlink.__contains__("http://") or not longlink.__contains__("."):
         return "invalid longlink"
     
@@ -61,7 +61,7 @@ def create_shortlink(longlink, email, delay):
             valid = True
 
     dt = str(datetime.datetime.now()).split(".")[0]
-    cursor.execute("INSERT INTO shortlinks VALUES (0, %s, %s, %s, %s, %s)", (shortlink, longlink, email, delay, dt))
+    cursor.execute("INSERT INTO shortlinks VALUES (0, %s, %s, %s, %s, %s)", (shortlink, longlink, secret, delay, dt))
     db.commit()
 
     return shortlink
@@ -86,7 +86,7 @@ def get(shortlink):
 @app.route("/api/create", methods=["POST"])
 def create():
     params = flask.request.get_json()
-    return create_shortlink(longlink=params["longlink"], email=params["email"], delay=params["delay"])
+    return create_shortlink(longlink=params["longlink"], secret=params["secret"], delay=params["delay"])
 
 if __name__ == "__main__":
     app.run("127.0.0.1", 2700)
